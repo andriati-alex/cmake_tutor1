b73d379c583373c7e98eb4784e6eb80591a776cf
andriati-alex <andriati.alex@gmail.com>
2021-07-04 21:36:47 -0300

    Added some comments to the header file trying to follow doxygen conventions

--- include/displaymsg.h
+++ include/displaymsg.h
@@ -1,4 +1,21 @@
 #include "msgtype.h"
 
-void display_msg(msg);              // replication of printf
-char get_indexed_char(msg, int);    // return positional character of string
+/*! This is the brief fool comment that must be a single line
+ *
+ * Just a silly implementation to use my custom datatype `msg`
+ *
+ * @param m Glorious datatype that is just pointer to char. I just like to
+ *          write some nonsense to have a longer description
+ */
+void display_msg(msg m);
+
+/*! This is the brief fool comment that must be a single line
+ *
+ * Not so useless function to get char at string index
+ *
+ * @param m Glorious datatype that is just pointer to char
+ * @param ind index to take the character
+ *
+ * @return character @c m[ind]
+ */
+char get_indexed_char(msg m, int ind);
